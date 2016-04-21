af3d2f36f5636f48da09f26761602cf436653538
dan <dan@ezoic.com>
2016-04-21 14:07:54 -0700

    fix the checkpoint update case to set server id

--- mysql_checkpoint_test.go
+++ mysql_checkpoint_test.go
@@ -26,12 +26,36 @@ func Test_MysqlCheckpointExists(t *testing.T) {
 	if err != nil {
 		t.Fatalf("cannot insert checkpoint into db manually, %s", err)
 	}
-	c := MysqlCheckpoint{AppName: "app", StreamName: "stream", TableName: "KinesisConnector.TestCheckpoint", Db: rc}
+	c := MysqlCheckpoint{AppName: "app", StreamName: "stream", TableName: "KinesisConnector.TestCheckpoint", Db: rc, ServerId: "testserverid"}
 
 	r := c.CheckpointExists("shard")
 
 	if r != true {
 		t.Errorf("CheckpointExists() = %v, want %v", false, r)
+	}
+
+	approximateArrivalTime := int(time.Now().Unix())
+	c.SetCheckpoint("shard", "fakeSeqNum", approximateArrivalTime)
+
+	rslt := rc.QueryRow("SELECT sequence_number, last_updated, last_arrival_time, server_id FROM KinesisConnector.TestCheckpoint WHERE checkpoint_key = ?", k)
+	var sequenceNumber, lastUpdated, serverId string
+	var lastArrivalTime int
+	err = rslt.Scan(&sequenceNumber, &lastUpdated, &lastArrivalTime, &serverId)
+	if err != nil {
+		t.Fatalf("cannot scan row for checkpoint key, %s", err)
+	}
+
+	if sequenceNumber != "fakeSeqNum" {
+		t.Errorf("SetCheckpoint() = %v, want %v", "fakeSeqNum", sequenceNumber)
+	}
+	if lastUpdated == "" {
+		t.Error("last_updated is empty")
+	}
+	if lastArrivalTime != approximateArrivalTime {
+		t.Errorf("last_arrival_time expected %v, actual %v", approximateArrivalTime, lastArrivalTime)
+	}
+	if serverId != "testserverid" {
+		t.Errorf("server_id expected %v, actual %v", "testserverid", serverId)
 	}
 
 	rc.Exec("DELETE FROM KinesisConnector.TestCheckpoint WHERE checkpoint_key = ?", k)
